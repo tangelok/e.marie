a3b93578bd6e735f4852b2a6f2477bc474194a71
Katie Angelo <krineangelo@gmail.com>
2017-10-05 17:02:12 -0400

    Updated Layout

--- js/main.js
+++ js/main.js
@@ -1,19 +1,1 @@
 
-
-
-
-
-
-/* ==========================================================================
-  About
-========================================================================== */
-
-//If you hover over the certain image
-  //Then it will display the title of the piece
-
-
-//If you hover over the hamburger
-    //Then the Nav bar will be displayed
-$('#hamburger').hover('on', function (){
-    $('nav').removeClass('hover');
-  });
